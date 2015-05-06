210c88236b10fa35fc7042eda5922c8b5a29a143
Luke Poskitt <luke.poskitt@gmail.com>
2015-05-07 00:22:22 +1000

    Parametised method support - GetDestinationsForRoute() returns success but is not yet deserialised.

--- lib/test.pl
+++ lib/test.pl
@@ -8,7 +8,11 @@ use Data::Dumper;
 
 my $p = WWW::PIDS->new;
 
-my @r = $p->GetDestinationsForAllRoutes();
+#my @r = $p->GetDestinationsForAllRoutes();
+
+#print Dumper( @r );
+
+#my @r = $p->GetDestinationsForAllRoutes();
+my @r = $p->GetDestinationsForRoute({ routeNo => 19 });
 
 print Dumper( @r );
-
