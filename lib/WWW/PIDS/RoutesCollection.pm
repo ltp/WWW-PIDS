963c3790c3c8f3cad8df798cd180cef047808425
Luke Poskitt <luke.poskitt@gmail.com>
2015-05-07 16:23:10 +1000

    Removed debug text/data

--- lib/WWW/PIDS/RoutesCollection.pm
+++ lib/WWW/PIDS/RoutesCollection.pm
@@ -28,23 +28,4 @@ sub new {
 
 __END__
 
-          'diffgram' => {
-                        'DocumentElement' => {
-                                             'ToReturn' => [
-                                                           {
-                                                             'InternalRouteNo' => '1',
-                                                             'DisplayAC' => 'false',
-                                                             'IsLowFloorTram' => 'false',
-                                                             'RouteNo' => '1',
-                                                             'HasSpecialEvent' => 'true',
-                                                             'AirConditioned' => 'true',
-                                                             'Destination' => 'East Coburg',
-                                                             'HeadboardRouteNo' => '1',
-                                                             'RequestDateTime' => '2015-05-07T16:01:48.8541456+10:00',
-                                                             'VehicleNo' => '2131',
-                                                             'HasDisruption' => 'false',
-                                                             'IsTTAvailable' => 'true',
-                                                             'SpecialEventMessage' => 'Route 1 & 8 Works 12-17 May: Trams operating in sections. Buses Brunswick Rd - Moreland Rd diverting around work.',
-                                                             'PredictedArrivalDateTime' => '2015-05-07T16:11:47+10:00'
-                                                           },
 
