7cadd412fec8c45daee1ff18f4b49c9c66331d04
Luke Poskitt <luke.poskitt@gmail.com>
2015-06-16 16:22:53 +1000

    Added WWW::PIDS::TramRunNoDetail module

---
+++ lib/WWW/PIDS/TramNoRunDetail.pm
@@ -0,0 +1,28 @@
+package WWW::PIDS::TramNoRunDetail;
+
+use strict;
+use warnings;
+
+our @ATTR = qw(AtLayover Available HasDisruption HasSpecialEvent HeadBoardRouteNo Up VehicleNo RouteNo);
+
+{
+	no strict 'refs';
+	*$_ = sub { return shift->{ $_ } } for @ATTR;
+}
+
+sub new {
+	my ( $class, $obj ) = @_;
+	my $self = bless {}, $class;
+
+	for my $a ( @ATTR ) {
+		defined $obj->{ $a} 
+			? $self->{ $a } = $obj->{ $a }
+			: die "Mandatory parameter $a not supplied in constructor" ;
+	}
+
+	return $self
+}
+
+1;
+
+__END__
