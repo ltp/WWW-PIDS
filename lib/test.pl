e2230aee122a892bd57c985561cd46923dcbda68
Luke Poskitt <luke.poskitt@gmail.com>
2015-06-16 16:28:36 +1000

    Updated test cases

--- lib/test.pl
+++ lib/test.pl
@@ -9,19 +9,17 @@ use Data::Dumper;
 my $p = WWW::PIDS->new;
 
 #my @r = $p->GetDestinationsForAllRoutes();
-
-#print Dumper( @r );
-
-#my @r = $p->GetDestinationsForAllRoutes();
-#my @r = $p->GetDestinationsForRoute( routeNo => 64 );
+#my @r = $p->GetNewClientGuid();
+#my @r = $p->GetDestinationsForRoute( routeNo => 1 );
 #my @r = $p->GetMainRoutes( );
-my @r = $p->GetMainRoutesForStop( stopNo => 3101 );
-#my @r = $p->GetRouteStopsByRoute( routeNo => 109, isUpDirection => 1);
+#my @r = $p->GetMainRoutesForStop( stopNo => 3101 );
+#my @r = $p->GetRouteStopsByRoute( routeNo => '1', isUpDirection => 1);
 #my @r = $p->GetListOfStopsByRouteNoAndDirection( routeNo => 3, isUpDirection => 0);
-#my @r = $p->GetNextPredictedArrivalTimeAtStopsForTramNo( tramNo => 64 );
+my @r = $p->GetNextPredictedArrivalTimeAtStopsForTramNo( tramNo => 59 );
 #my @r = $p->GetNextPredictedRoutesCollection( stopNo => 3101, routeNo => '3a', lowFloor => 0 );
 #my @r = $p->GetPlatformStopsByRouteAndDirection( routeNo => '3', isUpDirection => 0 );
 #my @r = $p->GetRouteSummaries();
+#my @r = $p->GetStopInformation( stopNo => 3101 );
 #my @r = $p->GetSchedulesCollection( stopNo => 3101, routeNo => '3a', lowFloor => 0, clientRequestDateTime => '2015-05-08T09:15:00' );
 #my @r = $p->GetSchedulesForTrip( tripID => 44652446, scheduledDateTime => '2015-05-08T09:15:00' );
 print Dumper( @r );
